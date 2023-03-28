5e7be8a439a1b9f5ae28ee7abab0d4cf820d6f99
thomascaldana <thomascaldana@gmail.com>
2023-03-28 15:15:40 -0300

    working

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 function setupTabs () {
-    document.querySelectorAll("tabs__button").forEach(button => {
+    document.querySelectorAll(".tabs__button").forEach(button => {
         button.addEventListener("click", () => {
             const sideBar = button.parentElement;
             const tabsContainer = sideBar.parentElement;
             const tabNumber = button.dataset.forTab;
             const tabToActivate = tabsContainer.querySelector(`.tabs__content[data-tab="${tabNumber}"]`)
 
+          sideBar.querySelectorAll(".tabs__button").forEach(button => {
+            button.classList.remove("tabs__button--active");
+          });
 
+          tabsContainer.querySelectorAll(".tabs__content").forEach(tab => {
+            tab.classList.remove("tabs__content--active");
+          });
 
-
-            console.log(sideBar)
-            console.log(tabsContainer)
-            console.log(tabNumber)
-            console.log(tabToActivate)
-         
-          
-        } )
+          button.classList.add("tabs__button--active");
+          tabToActivate.classList.add("tabs__content--active");
+        });
     })
 }
 
-    document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
     setupTabs();
-});
+})
